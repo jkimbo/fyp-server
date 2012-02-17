47c614e7a4ab7fefc5ae961b42cd08549ac6a373
Jonathan Kim <jkimbo@gmail.com>
2012-02-17 11:28:00 +0000

    Added google maps module

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
  */
 var express = require('express')
     , app = express.createServer()
-    , socket = require('socket.io')
+    , io = require('socket.io').listen(app)
+    , gm = require('googlemaps')
     , port = 1337;
 
 app.configure(function(){
@@ -13,13 +14,23 @@ app.configure(function(){
 });
 
 app.get('/', function(req, res) {
+    gm.distance('51.51957887606202,-0.16791701316833496', '51.498437793589694,-0.17423629760742188|51.50099581189912, -0.1252269744873047', function(err, data) {
+        console.log(JSON.stringify(data));
+    });
     res.send(req.params);
 });
 
 app.post('/location/:id', function(req, res) {
-    res.send(req.body);
+    res.json({hello:'world'});
 });
 
 app.listen(port);
 console.log("Express server listening on port %d", app.address().port);
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('data', { hello: 'world' });
+    socket.on('location', function(data) {
+        console.log(data);
+    });
+});
 
